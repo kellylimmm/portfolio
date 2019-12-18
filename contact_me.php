5e8e733233fb034d1c69cca9c1e805fa9c30cec6
“kellylimmm” <kellylim.06@gmail.com>
2019-12-18 16:00:31 +0800

    contact

--- contact_me.php
+++ contact_me.php
@@ -2,14 +2,14 @@
 
 if (isset($_POST['submit'])) {
 $name = $_POST['name'];
-$subject = $_POST['subject'];
-$mailFrom = $_POST['mail'];
+// $subject = $_POST['subject'];
+$mailFrom = $_POST['email'];
 $message = $_POST['message'];
 
 $mailTo = "kellylim.06@gmail.com";
 $headers = "From: ".$mailFrom;
 $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-mail($mailTo, $subject, $txt, $headers);
-header("Location: index.php?mailsend");
+mail($mailTo, $txt, $headers);
+header("Location: index.html?mailsend");
 }
